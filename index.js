a9731f171910f03cd13e948e8f62aa1f4a70a7fb
Jonathan Adams <jd.adams16@gmail.com>
2019-12-16 00:19:57 -0600

    Add info.

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    email: "jd.adams16@gmail.com",
+    github: "https://github.com/PieceMaker",
+    linkedin: "https://www.linkedin.com/in/jonathan-adams-b11a79121",
+    twitter: "https://twitter.com/RebelActuary"
+};
